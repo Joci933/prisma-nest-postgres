ab081a72cc7fc2df4cfb2fbb7bc91a463808497f
Mikus József <mikus.jozsef>
2023-03-02 08:26:23 +0100

    Basic validation added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,13 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Req() req: Request) {
-    console.log('asd', req);
+  signup(@Body() dto: AuthDto) {
     return this.authService.signup();
   }
 
