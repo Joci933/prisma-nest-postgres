385614f4c03b96c06ad9a9bcb4f02c54a24ba141
Mikus József <mikus.jozsef>
2023-03-02 08:46:15 +0100

    Migrations added, validate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -11,9 +14,30 @@ export class AuthService {
     };
   }
 
-  signup() {
-    return {
-      message: 'signup',
-    };
+  async signup(dto: AuthDto) {
+    // generate password
+    const hash = await argon.hash(dto.password);
+    // save user
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        /* select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        }, */
+      });
+      // Remove hash from the response
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
   }
 }
