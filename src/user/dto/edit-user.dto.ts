fcb21ffb4f0738f78ca04db245f928e007f28011
Mikus József <mikus.jozsef>
2023-03-02 14:29:55 +0100

    Update profile added

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class EditUserDto {
+  @IsEmail()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+}
